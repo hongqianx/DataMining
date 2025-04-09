a664d2e90778307b6bb0cddf8e28c8b4987cba7a
Hongqian Xia <xhq318@gmail.com>
2025-04-10 01:06:53 +0200

    change outliers to NA, update following dataframe df_expand -> df_trim -> df_agg_6h

--- Assignment 1/dataPrepare.py
+++ Assignment 1/dataPrepare.py
@@ -1,26 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 def printMetric(desc, metric):
     print(desc + " is " + str(metric) + ".")
 
-def trim_outliers(df, lower_percent=0.01, upper_percent=0.99):
-    numeric_cols = df.select_dtypes(include='number').columns
-    newdf = pd.DataFrame()
-    for col in numeric_cols:
-        lower_bound = df[col].quantile(lower_percent)
-        print(f"Lower bound for {col}: {lower_bound}")
-        upper_bound = df[col].quantile(upper_percent)
-        print(f"Upper bound for {col}: {upper_bound}")
-        newdf[col] = df[col].clip(lower=lower_bound, upper=upper_bound)
-    return newdf
+# for columns in data, change value <0, and upper 0.99 quantile as NA
+def trim_outliers(data, columns, upper_percent=0.99):
+    df_trim = data.copy()
+    for col in columns:
+        upper_bound = df_trim[col].quantile(upper_percent)
+        df_trim[col] = df_trim[col].where((df_trim[col] >= 0) & (df_trim[col] <= upper_bound), np.nan)
+    return df_trim
 
 
 
 # draw one box plot for multiple columns
-def box_plot(columns, fig_path):
-    df_expand[columns].plot(kind='box', figsize=(8, 5), vert=False, patch_artist=True,
+def box_plot(data, columns, fig_path):
+    data[columns].plot(kind='box', figsize=(8, 5), vert=False, patch_artist=True,
                                 boxprops=dict(facecolor='lightblue', color='gray'),
                                 medianprops=dict(color='red'),
                                 flierprops=dict(marker='o', markerfacecolor='orange', markersize=5, linestyle='none'))
@@ -36,7 +34,7 @@ def box_plot(columns, fig_path):
 def hist_plot(column, fig_path):
     plt.figure(figsize=(8, 5))
     plt.hist(column.dropna(), bins=30, color='skyblue', edgecolor='black')
-    plt.title('Histogram of your_column_name')
+    plt.title('Histogram')
     plt.xlabel('Value')
     plt.ylabel('Frequency')
     plt.grid(True, linestyle='--', alpha=0.5)
@@ -85,6 +83,7 @@ printMetric("Ratio of NA values", f"{df.isna().mean().mean() * 100:.4f}%")
 printMetric("Amount of duplicate records", df.duplicated().sum())
 
 # Timeframe range is from 2014-02-17 07:00 to 2014-06-09 00:00
+# ISO datetime formate
 df['time'] = pd.to_datetime(df['time'])
 min_time = df['time'].min()
 max_time = df['time'].max()
@@ -117,9 +116,6 @@ df_with_epoch = df.assign(epoch_time=(pd.to_datetime(df['time']) - pd.Timestamp(
 print(df_with_epoch['value'].corr(df_with_epoch['epoch_time']))
 
 # 2. create df_expand
-# ISO datetime formate
-df['time'] = pd.to_datetime(df['time'])
-
 # change 'variables' into columns, and 'value' are the value of columns
 df_tmp = df.pivot(columns='variable', values='value')
 df_expand = pd.concat([df[['id', 'time']].reset_index(drop=True), df_tmp.reset_index(drop=True)], axis=1)
@@ -141,12 +137,6 @@ info_uni = df_expand.nunique()
 hist_plot(column = df_expand['mood'], fig_path = "../image/mood.png")
 hist_plot(column = df_expand['activity'], fig_path = "../image/activity.png")
 
-# select numerical columns
-value_cols = df_expand.select_dtypes(include='number').columns
-
-trimmed_df = trim_outliers(df_expand)
-
-multi_hist_plot(columns=value_cols, fig_path="../image/trimmed_dist.png",data=trimmed_df )
 
 # 3.1 Box plot
 columns_to_plot1 = ['activity','circumplex.arousal', 'circumplex.valence','call','sms','mood']
@@ -156,16 +146,49 @@ columns_to_plot2 = ['appCat.builtin', 'appCat.communication','appCat.entertainme
                     'appCat.weather', 'screen']
 fig_path2 = "../image/box_plot2.png"
 
-box_plot(columns_to_plot1,fig_path1)
-box_plot(columns_to_plot2,fig_path2)
+box_plot(df_expand, columns_to_plot1,fig_path1)
+box_plot(df_expand, columns_to_plot2,fig_path2)
 
-# 4. aggregate data by day
-print(df_expand['time'].head())
+# 4. outlier
+# for ['mood','circumplex.arousal','circumplex.valence','activity','sms','call'], outlier is point outside of range in definition
+count_outside = df_expand[(df_expand['mood'] < 1) | (df_expand['mood'] > 10)].shape[0]
+print('mood has ' + str(count_outside) + ' outliers')
+count_outside = df_expand[(df_expand['circumplex.arousal'] < -2) | (df_expand['circumplex.arousal'] > 2)].shape[0]
+print('circumplex.arousal has ' + str(count_outside) + ' outliers')
+count_outside = df_expand[(df_expand['circumplex.valence'] < -2) | (df_expand['circumplex.valence'] > 2)].shape[0]
+print('circumplex.valence has ' + str(count_outside) + ' outliers')
+count_outside = df_expand[(df_expand['activity'] < 0) | (df_expand['activity'] > 1)].shape[0]
+print('activity has ' + str(count_outside) + ' outliers')
+count_outside = df_expand[~((df_expand['sms'] == 1) | (df_expand['sms'].isna()))].shape[0]
+print('sms has ' + str(count_outside) + ' outliers')
+count_outside = df_expand[~((df_expand['call'] == 1) | (df_expand['call'].isna()))].shape[0]
+print('call has ' + str(count_outside) + ' outliers')
+
+# for other columns, we delete value <0, and upper 0.99 quantile
+outlier_col = ['screen','appCat.builtin','appCat.communication',\
+           'appCat.entertainment', 'appCat.finance', 'appCat.game',\
+           'appCat.office', 'appCat.other', 'appCat.social', 'appCat.travel',\
+           'appCat.unknown', 'appCat.utilities', 'appCat.weather']
+
+
+df_trim = trim_outliers(data = df_expand, columns=outlier_col)
+# get value range
+info_stat = df_trim.describe(include="all")
+df_trim.to_csv('../input/df_trim.csv', index=False)
+
+# select numerical columns
+value_cols = df_trim.select_dtypes(include='number').columns
+
+multi_hist_plot(columns=value_cols, fig_path="../image/trimmed_dist.png",data=df_trim )
+
+
+# 5. aggregate data by day
+print(df_trim['time'].head())
 # df_expand['day'] = df_expand['time'].dt.date
 # df_expand['hour'] = df_expand['time'].dt.hour
 # group_cols = ['id', 'day', 'hour']
 
-df_expand['time_bin'] = df_expand['time'].dt.floor(f'{6}H')
+df_trim['time_bin'] = df_trim['time'].dt.floor(f'{6}H')
 group_cols = ['id', 'time_bin']
 
 # define aggregate function
@@ -192,13 +215,12 @@ agg_dict = {
 }
 
 # aggregate data by id and day
-df_agg = df_expand.groupby(group_cols).agg(agg_dict).reset_index()
+df_agg = df_trim.groupby(group_cols).agg(agg_dict).reset_index()
 
 df_agg.to_csv('../input/df_agg_6hour.csv', index=False)
 
-df_agg = pd.read_csv('../input/df_agg_hour.csv')
+# df_agg = pd.read_csv('../input/df_agg_hour.csv')
 # check missing value percentage
 missing_ratio = df_agg.isna().mean()
 # get value range
 info_stat = df_agg.describe(include="all")
-
