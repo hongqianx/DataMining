a49d29bec1ef8ca348b06a1dae2618d4706b601b
Hongqian Xia <xhq318@gmail.com>
2025-04-19 23:45:18 +0200

    clean code for task1

--- Assignment 1/Task 1.py
+++ Assignment 1/Task 1.py
@@ -1,23 +1,68 @@
 import numpy as np
 import pandas as pd
 from dataPrepare import DataPrepare
+data_proc = DataPrepare()
 
 # 1A
 # 1.read data
 input_data = r"../input/dataset_mood_smartphone.csv"
 df = pd.read_csv(input_data)
-data_proc = DataPrepare()
+
 # ISO datetime formate
 df['time'] = pd.to_datetime(df['time'])
+
+
+# 376912 records
+data_proc.printMetric("Amount of records", len(df))
+
+# NA ratio is 0.0107%
+data_proc.printMetric("Ratio of NA values", f"{df.isna().mean().mean() * 100:.4f}%")
+
+# Duplicate records is 0
+data_proc.printMetric("Amount of duplicate records", df.duplicated().sum())
+
+# Value mean, min and max
+data_proc.stat_col(data=df, column='value')
+
+# 27 unique users
+data_proc.printMetric("Amount of unique users", df["id"].nunique())
+
+# Time value correlation
+df_with_epoch = df.assign(epoch_time=(pd.to_datetime(df['time']) - pd.Timestamp('1970-01-01')) / pd.Timedelta('1s'))
+print(df_with_epoch['value'].corr(df_with_epoch['epoch_time']))
+
 
 # change 'variables' into columns, and 'value' are the value of columns
 df_expand = data_proc.data_pivot(data=df)
 
+# image distribution for all columns
+data_proc.hist_plot(column = df_expand['mood'], fig_path = "../image/mood.png")
+data_proc.hist_plot(column = df_expand['activity'], fig_path = "../image/activity.png")
+
+# Box plot
+columns_to_plot1 = ['activity','circumplex.arousal', 'circumplex.valence','call','sms','mood']
+fig_path1 = "../image/box_plot1.png"
+columns_to_plot2 = ['appCat.builtin', 'appCat.communication','appCat.entertainment', 'appCat.finance', 'appCat.game',\
+                    'appCat.office', 'appCat.other', 'appCat.social', 'appCat.travel','appCat.unknown', 'appCat.utilities',\
+                    'appCat.weather', 'screen']
+fig_path2 = "../image/box_plot2.png"
+
+data_proc.box_plot(df_expand, columns_to_plot1,fig_path1)
+data_proc.box_plot(df_expand, columns_to_plot2,fig_path2)
+
+
 # 1B
 # process outliers
 df_trim = data_proc.outlier_process(data=df_expand)
+# plot hist for numerical columns
+value_cols = df_trim.select_dtypes(include='number').columns
+data_proc.multi_hist_plot(columns=value_cols, fig_path="../image/trimmed_dist.png",data=df_trim )
+
+
 # aggregate data
 df_agg = data_proc.data_aggregate(data=df_trim)
+# check missing value percentage
+print(df_agg.isna().mean())
 
 # 1C
 df_agg['id'] = df_agg['id'].astype('category').cat.codes
